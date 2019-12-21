632533d9a36aa441124ca870f25bc28c3bc5ae6b
Darwin <invintrar@gmail.com>
2019-12-21 15:00:01 -0500

    Avance en el nrf

--- principal.py
+++ principal.py
@@ -15,9 +15,14 @@ spi.mode = 0
 gpio.setwarnings(False)
 # Set mode: BOARD pines physical, BCM pines leveles
 gpio.setmode(gpio.BOARD)
+
 led = 7
 ce = 15
 irq = 16
+espera_ack = 0
+bIntEx = 0
+ve=[0]
+ACK = 0
 
 gpio.setup(led, gpio.OUT, initial = gpio.LOW)
 gpio.setup(ce,gpio.OUT, initial = gpio.LOW)
@@ -51,26 +56,94 @@ def configNrf():
 	sleep(2)
 	gpio.output(ce, gpio.HIGH)
 	sleep(0.15)
-def envia_datos():
+
+def sendData(data):
 	gpio.output(ce, gpio.LOW)
 
 	#STATUS
 	spi.xfer2([0x27, 0x70])
 
 	#W_TX_PAYLOAD
-	 
+	 ve[0]=0xA0
+	 for x in data:
+	 	ve.append(x)
+
+	 spi.xfer2(ve)
+
+	 #CONFIG Activo mode transmition
+	 spixfer2(0x20,0x0E)
+
+	 gpio.output(ce, gpio.HIGH)
+	 sleep(0.015)
+	 gpio.output(ce, gpio.LOW)
+
+	 while(bIntEx == 0):
+	 	if(espera_ack == 400):
+	 		break
+
+	 #STATUS
+	 resp = spi.xfer2([0x07,0x00])
+
+	 #TX_FLUSH Limpia el buffer FIFO TX
+	 spi.xfer2([0xE1])
+
+	 #STATUS Clear register
+	 spi.xfer2([0x27, 0x70])
+
+	 #CONFIG configuramos modo recepcion
+	 spi.xref2([0x20, 0x0F])
+
+	 gpio.output(ce, gpio.HIGH)
+
+	 sleep(0.15)
+
+def recData():
+	ve=[0x00]
+	#STATUS
+	status = spi.xref2([0x07,0x00])
+
+	spi.xref2([0x27,0x70])
+	if((status[1] & 0x40) == 1):
+		#R_RX_PAYLOAD recibe datos del buffer FIFO RX
+		 ve[0]=0x61
+	 	 for x in range(10):
+	 	 	ve.append(0x00)
+
+	 	 spi.xfer2(ve)
+
+	 	 return ve
+
 
 def intEx(channel):
-	print("Interrupcion")
+	recData()
 
 gpio.add_event_detect(irq,gpio.FALLING, callback = intEx, bouncetime = 300)
+
+configNrf()
 
 try:
 	while True:
 		gpio.output(led,gpio.HIGH)
-		sleep(0.2);
+		sleep(0.25);
 		gpio.output(led, gpio.LOW)
-		sleep(0.2)
+		sleep(0.25)
+		resp=spi.xref2([0x07, 0x00])
+
+		data=[0,1,1,2,3,4,5,6,7,8,9]
+
+		#config
+		resp1 = spi.xref2([0x00,0x00])
+
+		#EN_RXADDR
+		resp2 = spi.xrfe2([0x02,0x00])
+
+		#RX_PW0
+		resp3 = spi.xref2([0x0A,0x00])
+
+		print("%0x00:%s 0x02:%s 0x0A:%s" % resp[1], resp1[1], resp2[1], resp3[1])
+
+		ACK = sendData(data)
+
 except KeyboardInterrupt:
 	#gpio.output(led,gpio.LOW)
 	spi.close()
